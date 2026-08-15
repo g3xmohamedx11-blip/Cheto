b65261f2c47c6559cd15da6d90ead99ede885749
g3xmohamedx11-blip <g3xmohamedx11@gmail.com>
2026-08-15 15:17:10 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VODKA-CHET0"
+include(":app")
